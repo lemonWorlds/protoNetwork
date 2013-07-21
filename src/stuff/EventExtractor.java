e927fd2d2f541b330ddb4b4d7f006151dd3263f4
tohaga01 <tohaga01@cspc546.dcs.bbk.ac.uk>
2013-07-21 16:37:18 +0100

    Revert "Tested extractor."
    This reverts commit 6859773f2aab39654b75bf468e5af0c07610fb79.

--- src/stuff/EventExtractor.java
+++ src/stuff/EventExtractor.java
@@ -1,34 +1,12 @@
 package stuff;
 
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 
 public class EventExtractor {
-	//private static final InfModel schema = SchemaSingleton.getSchema();
-	private static final String queryStr = "PREFIX ex: <http://www.model.org/> " +
-			                  "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-			                  "SELECT ?type " +
-			                  "WHERE {" +
-			                  "      ?ev ex:occurredAt ?peer . " +
-			                  "      ?ev rdf:type ?type" +
-			                  "}";
-	private static Query query = null;
-	
-	public EventExtractor() {
-		query = QueryFactory.create(queryStr);
-	}
-	
-	public String extract(Model model) {
-		QueryExecution execution = QueryExecutionFactory.create(query,model);
-		ResultSet results = execution.execSelect();
-		while (results.hasNext()) {
-			System.out.println(results.next());
-		}
+	public static String extract(Model model) {
+		
+		
+		
 		return null;
 	}
 }
